80e8d61cefd672498d74a9f2449c717e7d7e6b27
Doro T <do.therrien@outlook.com>
2024-06-24 11:34:29 -0400

    Travail sur la navbar et le footer pour qu'ils soient dans le projet react.js

---
+++ src/components/Footer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../styles/Navbar.css' // Custom CSS for Footer
+
+const Footer = () => {
+    return (
+      <>
+        <hr />
+        <footer className="background-color">
+          <div className="container-fluid">
+            <div className="row m-0">
+              <div className="col-md-4 col-sm-12 text-center text-md-start">
+                <h5 className="text-md-start font-weight-bold">Quick links</h5>
+                <ul className="list-unstyled text-md-start">
+                  <li><a href="#home">Home</a></li>
+                  <li><a href="#about">About</a></li>
+                  <li><a href="#actors">Actors</a></li>
+                  <li><a href="#movies">Movies</a></li>
+                </ul>
+              </div>
+              <div className="col-md-4 col-sm-12 text-center text-md-start">
+                <h5 className="text-center font-weight-bold">Follow us !</h5>
+                <div className="social-icons text-center pt-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" viewBox="0 0 512 512">
+                    <path fill="#577063" d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="37" width="37" viewBox="0 0 320 512">
+                    <path fill="#577063" d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5 16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                  </svg>
+                </div>
+                <div className="footer-contact text-center pb-3">
+                  <p className="pb-3">contact@thema.ca</p>
+                  <p>+1 (555) 123-4567</p>
+                </div>
+              </div>
+              <div className="col-md-4 col-sm-12 text-center ">
+                <div className="footer-logo text-md-end">
+                  <img src="logo.png" alt="Your Logo" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+        <div className="footer text-center">&copy;<span id="year">2024</span><span> ThéMa. All rights reserved.</span></div>
+      </>
+    );
+  }
+  
+
+export default Footer;
---
+++ src/components/Navigation.js
@@ -0,0 +1,88 @@
+
+import '../styles/Navbar.css' // Custom CSS for Navbar
+
+import React, { useEffect } from 'react';
+
+const Navbar = () => {
+    useEffect(() => {
+      const bootstrapScript = document.createElement('script');
+      bootstrapScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js';
+      bootstrapScript.async = true;
+      document.body.appendChild(bootstrapScript);
+  
+      return () => {
+        document.body.removeChild(bootstrapScript);
+      };
+    }, []);
+  
+    return (
+      <div className="navbar-offset">
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top">
+          <div className="container-fluid navbar-container mx-auto">
+            <h4><a className="navbar-brand" href="#" style={{ color: '#D7BE82' }}>ThéMa</a></h4>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto ms-auto mb-2 mb-lg-0">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle text-uppercase" href="#" id="navbarDropdown" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    Movies
+                  </a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <ul>
+                      <li><a className="dropdown-item" href="#">Action & Adventure</a></li>
+                      <li><a className="dropdown-item" href="#">Action</a></li>
+                      <li><a className="dropdown-item" href="#">Adventure</a></li>
+                      <li><a className="dropdown-item" href="#">Crime</a></li>
+                      <li><a className="dropdown-item" href="#">Thriller</a></li>
+                      <li><a className="dropdown-item" href="#">War</a></li>
+                      <li><a className="dropdown-item" href="#">Western</a></li>
+                    </ul>
+                    <ul>
+                      <li><a className="dropdown-item" href="#">Science Fiction & Fantasy</a></li>
+                      <li><a className="dropdown-item" href="#">Science Fiction</a></li>
+                      <li><a className="dropdown-item" href="#">Fantasy</a></li>
+                      <li><a className="dropdown-item" href="#">Horror</a></li>
+                    </ul>
+                    <ul>
+                      <li><a className="dropdown-item" href="#">Drama & Romance</a></li>
+                      <li><a className="dropdown-item" href="#">Drama</a></li>
+                      <li><a className="dropdown-item" href="#">Romance</a></li>
+                      <li><a className="dropdown-item" href="#">History</a></li>
+                    </ul>
+                    <ul>
+                      <li><a className="dropdown-item" href="#">Various</a></li>
+                      <li><a className="dropdown-item" href="#">Comedy</a></li>
+                      <li><a className="dropdown-item" href="#">Animation</a></li>
+                      <li><a className="dropdown-item" href="#">Documentary</a></li>
+                      <li><a className="dropdown-item" href="#">Family</a></li>
+                      <li><a className="dropdown-item" href="#">Mystery</a></li>
+                      <li><a className="dropdown-item" href="#">Music</a></li>
+                      <li><a className="dropdown-item" href="#">TV Movie</a></li>
+                    </ul>
+                  </div>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-uppercase" href="#">Actors</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-uppercase" href="#">About</a>
+                </li>
+              </ul>
+              <form className="d-flex" role="search">
+                <input className="form-control me-2 custom-placeholder" type="search" placeholder="Research" aria-label="Search" />
+                <button className="btn btn-outline-light custom-button" type="submit">
+                  <i className="fa-solid fa-magnifying-glass" style={{ color: '#d4c087' }}></i>
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+  
+  export default Navbar;
