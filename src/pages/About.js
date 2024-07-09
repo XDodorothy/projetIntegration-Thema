20d903fb463bc42a5d3f4e2d94aca39805a8aaa0
Doro T <do.therrien@outlook.com>
2024-07-09 11:39:05 -0400

    Amélioration des pages About/Apropos

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,34 +1,40 @@
 import '../styles/about.css'; 
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
+import { useHistory } from 'react-router-dom';
 
 
 
 
 const About = () => {
+    const history = useHistory();
+
+    const navigateToApropos = () => {
+        history.push('/apropos');
+    };
 
   return (
     <div>
       <Navigation />
       <div>
       <div className="container-fluid py-5 champ">
-        <div className="row align-items-center">
-          <div className="col-12 col-md-4 mb-3 mb-md-0 text-center mx-auto">
+        <div className="row d-flex justify-content-center">
+          <div className="col-12 col-md-4 mb-3 text-end">
             <img
               src="images/Dorothee.jpg"
               className="img-fluid rounded actor-image"
               alt="Dorothee"
             />
           </div>
-          <div className="col-12 col-md-7 text-center mx-auto">
+          <div className="col-12 col-md-4 text-center ">
           <img
               src="images/Hamanou.jpg"
               className="img-fluid rounded actor-image"
-              alt="Hhamanou"
+              alt="Hamanou"
             />
           </div>
         </div>
-        <div className='col'>
+        <div className='col d-flex justify-content-center'>
             <p className="py-3 biography text-justify">
              Dorothée and Hamanou, two students passionate about web development, joined forces to create an online movie catalog site. Armed with the knowledge they acquired during their Web Development program at O'Sullivan College of Quebec, they used HTML, CSS, JavaScript, JSON, and React to bring this project to life. Hamanou primarily focused on the site's structure and design, ensuring an attractive and intuitive user interface. Meanwhile, Dorothée handled the site's dynamics, using JavaScript and React to integrate interactive features and optimize the user experience. Together, they leveraged JSON to structure and manage the movie data from The Movie Database (TMDB), creating a high-performing and captivating site for movie enthusiasts.
 
@@ -37,8 +43,8 @@ ThéMa is derived from parts of their names. The first part takes the final syll
             </div>
       </div>
       <div className="centerer">
-              <div className="wrap">
-                <a className="btn-5" href="Apropos.js">
+              <div className="wrap" onClick={navigateToApropos}>
+                <a className="btn-5" href='#'>
                   See french version
                 </a>
               </div>
--- src/pages/Apropos.js
+++ src/pages/Apropos.js
@@ -1,35 +1,42 @@
 import '../styles/about.css'; 
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
+import { useHistory } from 'react-router-dom';
 
 
 
 
 const Apropos = () => {
+  const history = useHistory();
+
+  const navigateToAbout = () => {
+      history.push('/about');
+  };
+
 
   return (
     <div>
       <Navigation />
       <div>
       <div className="container-fluid py-5 champ">
-        <div className="row align-items-center">
-          <div className="col-12 col-md-4 mb-3 mb-md-0 text-center mx-auto">
+        <div className="row d-flex justify-content-center">
+          <div className="col-12 col-md-4 mb-3 text-end">
             <img
               src="images/Dorothee.jpg"
               className="img-fluid rounded actor-image"
               alt="Dorothee"
             />
           </div>
-          <div className="col-12 col-md-7 text-center mx-auto">
+          <div className="col-12 col-md-4 text-center ">
           <img
               src="images/Hamanou.jpg"
               className="img-fluid rounded actor-image"
-              alt="Hhamanou"
+              alt="Hamanou"
             />
           </div>
         </div>
-        <div className='col'>
-            <p className="py-3 biography text-justify">
+        <div className='col d-flex justify-content-center'>
+            <p className="py-3 biography text-justify ">
             Dorothée et Hamanou, deux étudiants passionnés de développement web, ont uni leurs forces pour créer un site de catalogue de films en ligne. Forts de leurs connaissances acquises au cours de leur programme en Développement web au Collège O'Sullivan de Québec, ils ont utilisé les langues HTML, CSS, JavaScript, JSON et React pour donner vie à ce projet. Hamanou s'est concentré surtout sur la structure et le design du site, assurant une interface utilisateur attrayante et intuitive grâce. De son côté, Dorothée a géré plus au niveau de la dynamique du site, en utilisant JavaScript et React pour intégrer des fonctionnalités interactives et optimiser l'expérience utilisateur. Ensemble, ils exploitent JSON pour structurer et gérer les données des films de la base de donned de The Movie Database (TMDB), créant ainsi un site performant et captivant pour les cinéphiles.
 
 ThéMa vient d'une part et de l'autre de cette équipe. La première part prend la syllable finale de Dorothée et la seconde, la centrale de Hamanou. Ce nom convenait aux deux partis et s'arrimait bien avec le theme de ce projet.
@@ -39,9 +46,9 @@ ThéMa vient d'une part et de l'autre de cette équipe. La première part prend 
 
       </div>
       <div className="centerer">
-              <div className="wrap">
-                <a className="btn-5" href="About.js">
-                  Revenir à la version originale
+              <div className="wrap" onClick={navigateToAbout}>
+                <a className="btn-5" href='#'>
+                  Version originale
                 </a>
               </div>
               </div>
